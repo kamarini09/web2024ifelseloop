4ec5e1b7ad455802201284a8e207ca3a8e4fb079
kamarini <>
2024-09-09 13:03:41 +0200

    end for today

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 // Get the container element
-
+const list = document.getElementById("number-list");
 
 // Loop through numbers from 1 to 20
 
-    // Create a new div element for each number
+const  myNumbers = () => { 
+    let numbers = [];
+    for (i=1; i<=20; i++){
+    numbers.push(i);
+}
+return numbers
+}
+const numbers = myNumbers();
+// Create a new div element for each number
+   
+    numbers.forEach(x=>{
+        const newDiv = document.createElement("div");
+        //newDiv.textContent =`${ numbers[numbers.indexOf(x)]}`
+        newDiv.textContent =`${x}`
+
+        newDiv.classList.add("number");
+         // Check if the number is even or odd and apply the appropriate class
+   if(x %2 !==0){
+    newDiv.classList.add("odd")
+   } else {
+    newDiv.classList.add("even")
+   }
+  // Add an event listener to highlight the number when clicked
+   newDiv.addEventListener("click", () => {
+    newDiv.classList.add("highlight")
+   })
+   
+// Append the new div to the container
     
-    // Check if the number is even or odd and apply the appropriate class
+   list.appendChild(newDiv);
+       
+    })
     
-    // Add an event listener to highlight the number when clicked
+   
+
+    
+    
+   
 
         // Remove highlight from all numbers
         
         // Add highlight to the clicked number
     
-    // Append the new div to the container
     
 
